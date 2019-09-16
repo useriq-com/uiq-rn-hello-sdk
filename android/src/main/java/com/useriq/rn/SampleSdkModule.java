15605e77cac5fecbac75b290707767f646956553
Palkesh Jain <Palkesh.jain@useriq.com>
2019-09-16 17:17:05 +0530

    added sdk for android

--- android/src/main/java/com/useriq/rn/SampleSdkModule.java
+++ android/src/main/java/com/useriq/rn/SampleSdkModule.java
@@ -1,29 +1,27 @@
 package com.useriq.rn;
 
 import android.content.Context;
-import android.widget.Toast;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.useriq.sample_sdk.Main;
 
 public class SampleSdkModule extends ReactContextBaseJavaModule {
 
-    private static final String TAG = "Sample-sdk";
-    private final ReactApplicationContext reactContext;
+    private static final String TAG = "sample_sdk";
 
     public SampleSdkModule(ReactApplicationContext reactContext) {
         super(reactContext);
-        this.reactContext = reactContext;
     }
 
     @Override
     public String getName() {
-        return "UseriqReactNative";
+        return "sample_sdk";
     }
 
     @ReactMethod
     public void init(Context context) {
-        Toast.makeText(context, "Hello", Toast.LENGTH_LONG).show();
+        new Main().showDialog(context);
     }
 }
