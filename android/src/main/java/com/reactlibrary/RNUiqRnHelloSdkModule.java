50d9555a32aac9e75031d7a900204afb8f5cf69c
Palkesh Jain <Palkesh.jain@useriq.com>
2019-09-16 20:45:55 +0530

    updated android api

--- android/src/main/java/com/reactlibrary/RNUiqRnHelloSdkModule.java
+++ android/src/main/java/com/reactlibrary/RNUiqRnHelloSdkModule.java
@@ -1,22 +1,26 @@
-
 package com.reactlibrary;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
 
 public class RNUiqRnHelloSdkModule extends ReactContextBaseJavaModule {
 
-  private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-  public RNUiqRnHelloSdkModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    public RNUiqRnHelloSdkModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
 
-  @Override
-  public String getName() {
-    return "RNUiqRnHelloSdk";
-  }
+    @Override
+    public String getName() {
+        return "RNUiqRnHelloSdk";
+    }
+
+    @ReactMethod
+    public void showMessage() {
+        new Main().showToast(reactContext);
+    }
+
 }
